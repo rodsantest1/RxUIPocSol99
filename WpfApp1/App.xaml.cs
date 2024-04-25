c1ce0b86d89c75510179e5f5295fa91e3068d307
Rodney Santiago <rodney@rcsan96.onmicrosoft.com>
2024-04-24 23:42:10 -0400

    MVP RUI Xaml

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System.Configuration;
+﻿using ReactiveUI;
+using Splat;
+using System.Configuration;
 using System.Data;
+using System.Reflection;
 using System.Windows;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        public App()
+        {
+            // A helper method that will register all classes that derive off IViewFor 
+            // into our dependency injection container. ReactiveUI uses Splat for it's 
+            // dependency injection by default, but you can override this if you like.
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+        }
     }
-
 }
